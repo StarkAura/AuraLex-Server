86b7334f8106ec047cdb5085a467b9f5ef6249c1
Stephanie Nwankwo <stephybelle208@gmail.com>
2025-06-03 15:05:43 +0100

    feat: Implement GET Endpoint to List All Onboarded Student

--- routes/onboarding.route.js
+++ routes/onboarding.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { onboardStudent } = require('../controllers/onboarding');
+const { onboardStudent, getAllStudents } = require('../controllers/onboarding');
 const { validateStudent } = require('../middlewares/onboarding');
 
 router.post('/onboard', validateStudent, onboardStudent);
+router.post('/', validateStudent, getAllStudents);
 
 module.exports = router;
